4a638bddd55dbf6521870d2f713552e850c62eab
mattrachwal <mattrachwal@gmail.com>
2018-03-29 14:57:28 -0500

    Updated the Elastic dependency to support 5.2 api bug fixes  * Bumped version to 1.0.1

--- test/config.js
+++ test/config.js
@@ -4,7 +4,7 @@ module.exports = {
         host: process.env.ES_HOST || '192.168.99.100:9200', //hostname:port of elastic instance(s)
         requestTimeout: 120000, // how long to wait until the response is assumed to be abandoned
         log: 'warning', // logging level, one of: error, warning, info, debug, trace
-
+        apiVersion: process.env.ES_VERSION,
         // the index the service should manage
         index: {
             name: 'unit_test',
